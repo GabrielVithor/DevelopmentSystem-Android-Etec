496be65cf6cfe22fb4a134585d6f8b56ec5e935a
Gabriel Vithor <102717398+GabrielVithor@users.noreply.github.com>
2022-04-02 07:17:30 -0300

    jogo-da-velha-1.0

---
+++ Jogodavelha/app/src/main/java/com/example/jogodavelha/splash.java
@@ -0,0 +1,33 @@
+package com.example.jogodavelha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivty();
+            };
+        },4000);
+    }
+
+    private void mostrarMainActivty(){
+        Intent it = new Intent(splash.this, MainActivity.class);
+        startActivity(it);
+        finish();
+    }
+
+}
