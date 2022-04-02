ac920a95b4635fdd5e531f77801247134ee56660
Gabriel Vithor <102717398+GabrielVithor@users.noreply.github.com>
2022-04-02 04:04:37 -0300

    project-class220402

---
+++ allProjects/AppWidgetProvider/app/src/main/java/com/example/appwidgetprovider/App_widget_provider.java
@@ -0,0 +1,14 @@
+package com.example.appwidgetprovider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class App_widget_provider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_widget_provider);
+    }
+}
